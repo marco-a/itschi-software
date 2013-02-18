9b4eb94709b2540e1d9dc9edecf9c043480c63f3
bluefirex <mail@gidix.de>
2013-02-18 18:09:39 +0100

    Added menu-functions and PHPDoc

--- lib/template.php
+++ lib/template.php
@@ -13,6 +13,22 @@
 		private static $end = false;
 		private static $debug = false;
 
+		/**
+		 *	@name 	menu
+		 *			Holds all menu-items so plugins can add some.
+		 */
+
+		protected static $menu = array(
+			'index'			=>	'Startseite',
+			'forum'			=>	'Forum',
+			'memberlist'	=>	'Mitglieder'
+		);
+
+		/**
+		 *	@name 	style
+		 *			Holds style-directory and style-directory's name.
+		 */
+
 		public static $style = array(
 			'dir'	=>	'standard',
 			'dirName'	=>	'standard',
@@ -39,6 +55,14 @@
 			self::$root = $root;
 		}
 
+		/**
+		 *	@name 	logError
+		 *			prints an error
+		 *
+		 *	@param 	string $error
+		 *	@return void
+		 */
+
 		public static function logError($error) {
 			echo '
 				<link rel="stylesheet" href="styles/error.css" />
@@ -61,6 +85,15 @@
 			exit;
 		}
 
+		/**
+		 *	@name 	assign
+		 *			Assigns one ore more variables for use in a template.
+		 *
+		 *	@param 	mixed $vars
+		 *	@param 	string $value
+		 *	@return true
+		 */
+
 		public static function assign($vars, $value = '') {
 			if (is_array($vars)) {
 				foreach($vars as $k => $v) {
@@ -73,40 +106,114 @@
 			return true;
 		}
 
+		/**
+		 *	@name 	assignBlock
+		 *			Assigns an array (a block) for use in a template (mainly for foreach()).
+		 *
+		 *	@param 	string $name
+		 *	@param 	array $values
+		 *	@return true
+		 */
+
 		public static function assignBlock($name, $values) {
 			self::$blocks[$name][] = $values;
 			return true;
 		}
 
+		/**
+		 *	@name 	getVar
+		 *			Returns the value of a variable assigned with assign.
+		 *	
+		 *	@param 	string $var
+		 *	@return string
+		 */
+
 		public static function getVar($var) {
 			return self::$vars[$var];
 		}
+
+		/**
+		 *	@name 	getPage
+		 *			Gets the name of the file currently displayed.
+		 *
+		 *	@return string
+		 */
 
 		public static function getPage() {
 			return self::getVar('PAGE');
 		}
 
+		/**
+		 *	@name 	getStyleDirName
+		 *			Returns style's directory name.
+		 *
+		 *	@return string
+		 */
+
 		public static function getStyleDirName() {
 			return self::$style['dirName'];
 		}
 
-		public static function getMenu() {
-			return array(
-				'index'			=>	'Startseite',
-				'forum'			=>	'Forum',
-				'memberlist'	=>	'Mitglieder'
-			);
+		/**
+		 *	@name 	addToMenu
+		 *			Adds an item to the template's menu.
+		 *
+		 *	@param 	string $title
+		 *	@param 	string $link
+		 *	@return boolean
+		 */
+
+		public static function addToMenu($title, $link) {
+			if (!preg_match('^(ht|f)tps?\:\/\/^', $link)) {
+				self::$menu[$link] = $title;
+				return true;
+			}
+
+			return false;
 		}
+
+		/**
+		 *	@name 	getMenu
+		 *			Returns an array containing all menu items for use in a template.
+		 *
+		 *	@return array
+		 */
+
+		public static function getMenu() {
+			return self::$menu;
+		}
+
+		/**
+		 *	@name 	end
+		 *			Prohibits more templates to display.
+		 *
+		 *	@return void
+		 */
 
 		public static function end() {
 			self::$end = true;
 		}
 
+		/**
+		 *	@name 	isDebug
+		 *			Returns true if template-system is in debug-mode.
+		 *
+		 *	@return boolean
+		 */
+
 		public static function isDebug() {
 			return self::$debug;
 		}
 
-		public static function display($section, $acp = false) {
+		/**
+		 *	@name 	display
+		 *			Displays a template-file.
+		 *
+		 *	@param 	string $section
+		 * 	@return void
+		 */
+
+		public static function display($section) {
 			global $db, $user, $config, $token;
 
 			if (!self::$end) {
