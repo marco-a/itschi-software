3215c056b746357b060a797aede806bd43563151
bluefirex <mail@gidix.de>
2013-02-18 17:32:20 +0100

    added note

--- styles/standard/index.php
+++ styles/standard/index.php
@@ -5,7 +5,8 @@
 <h1 class="title">News</h1>
 
 <div id="news">
-	Coming soon.
+	Coming soon.<br />
+	<a href="http://gidix.de/user/1/">bluefirex</a> wird dieses Feature nachreichen.
 </div>
 
 <?php
