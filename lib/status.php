389f6857ac540013d83e65a125e94cd57ee36ff7
PatrickWe <weber.patrick91@gmail.com>
2013-02-18 15:48:48 +0100

    Little Syntax update

--- lib/status.php
+++ lib/status.php
@@ -2,7 +2,11 @@
 	require '../base.php';
 	
 	$statusUpdate = $db->chars(htmlspecialchars($_POST['status']));
-	$db->query("UPDATE itschi_users SET user_status = '".$statusUpdate."' WHERE user_id = '".$user->row['user_id']."'");
+	$db->query("
+		UPDATE itschi_users
+		SET user_status = '" . $statusUpdate . "'
+		WHERE user_id = " . (int)$user->row['user_id']
+	);
 	
 	die('Status gespeichert.');
 ?>
