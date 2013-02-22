03a86f9faf4c48ad9848dba96ada2501b572b35b
Marco Agnoli <marco.agnoli@me.com>
2013-02-22 22:56:18 +0100

    remove example

--- lib/plugins/plugin.HTTP.php
+++ lib/plugins/plugin.HTTP.php
@@ -9,7 +9,7 @@
 	*	Editing may cause your cat to be eaten by your subwoofer
 	*	or serious frozen air around your head.
 	*	It may also cause headaches.
-	*   Dafuq did I just read?
+	*	Dafuq did I just read?
 	*/
 
 	$_root = dirname(__FILE__).DIRECTORY_SEPARATOR.'HTTP'.DIRECTORY_SEPARATOR;
@@ -36,67 +36,4 @@
 
 	// init HTTP
 	HTTP::init();
-
-	/*
-		+---------+
-		| example |
-		+---------+
-	*/
-	/*
-	$HTTPRequest = HTTPRequest::alloc(HTTP::OPT_METHOD_POST | HTTP::OPT_MULTIPART);
-
-	$HTTPRequest->setOpt(HTTP::OPT_HOST, 'pro-fusion.ch');
-	$HTTPRequest->setOpt(HTTP::OPT_REQ_FILE, 'test_http.php');
-
-	$HTTPRequestData = HTTPRequestData::alloc();
-	$HTTPRequestData->add('getParam', 'value');
-	$HTTPRequestData->add('anotherGetParam', 'test');
-	$HTTPRequestData->addFile('logo', '../../styles/standard/images/icons/topics/topic.png');
-
-	$HTTPRequest->setOpt(HTTP::OPT_DATA, $HTTPRequestData);
-
-	$HTTPRequest->send(function(HTTPResponse $response) {
-		if ($response->getErrorCode() == 0) {
-			echo 'responseCode: '.$response->getResponseCode().PHP_EOL;
-			echo 'mimeType: '.$response->getMimeType().PHP_EOL;
-			echo 'response: '.$response->getResponse();
-		} else {
-			echo 'an error occurred: '.$response->getErrorString();
-		}
-	});
-
-
-	EXAMPLE OUTPUT:
-
-
-	responseCode: 200
-	mimeType: text/html
-	response: 121
-
-	post:
-
-	Array
-	(
-	    [getParam] => value
-	    [anotherGetParam] => test
-	)
-
-	files:
-
-	Array
-	(
-	    [logo] => Array
-	        (
-	            [name] => topic.png
-	            [type] => image/png
-	            [tmp_name] => [tmp dir]
-	            [error] => 0
-	            [size] => 1327
-	        )
-
-	)
-
-	0
-
-	*/
 ?>
