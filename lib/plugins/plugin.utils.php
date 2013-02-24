2087121805272cdbbae70723fdc7d8f047f9f62f
Marco Agnoli <marco.agnoli@me.com>
2013-02-24 02:48:11 +0100

    updated utils

--- lib/plugins/plugin.utils.php
+++ lib/plugins/plugin.utils.php
@@ -18,6 +18,11 @@
 
 	final class utils extends plugin {
 		/*
+			@arrayIndexCache
+		*/
+		private static $arrayIndexCache = array();
+
+		/*
 			String functions
 		*/
 
@@ -37,14 +42,86 @@
 			return mb_strtolower($str, CHARSET);
 		}
 
-		public function strLength($str) {
-			return mb_strlen($str, CHARSET);
+		public function strLength($str, $raw = FALSE) {
+			return ($raw ? strlen($str) : mb_strlen($str, CHARSET));
 		}
 
 		public function strSubstr($str, $start, $length = NULL) {
 			if ($length == NULL) $length = $this->strLength($str);
 
 			return mb_substr($str, $start, $length, CHARSET);
+		}
+
+		/*
+			Array functions
+		*/
+
+		/*
+			@name	arrayClearIndexCache
+
+			@author	marco.a
+
+			@return	void
+		*/
+		public static function arrayClearIndexCache() {
+			self::$arrayIndexCache = array();
+		}
+
+		/*
+			@name	arrayGetIndexCache
+
+			@author	marco.a
+
+			@return	array
+		*/
+		public static function arrayGetIndexCache() {
+			return (is_array(self::$arrayIndexCache) ? self::$arrayIndexCache : array());
+		}
+
+		/*
+			@name	arraySearchIndexByValue
+
+			@author	marco.a
+
+			@param	array	$array	the array to search in
+			@param	mixed	$value	value to be searched
+			@param	bool	$strictType	enable or disable strict value comparison between $value and array item [disabled]
+			@param	bool	$collectIndexes	immediately return first found index [enabled]
+			@param	string	$cacheName	cache result in an array [empty -> disabled]
+
+			@return	mixed	index, -1 when not found or array (when multiple indexes are found and $collectIndexes is TRUE)
+		*/
+		public static function arraySearchIndexByValue($array, $value, $strictType = FALSE, $collectIndexes = TRUE, $cacheName = '') {
+			if (!empty($cacheName)) {
+				if (!isset(self::$arrayIndexCache[$cacheName])) {
+					self::$arrayIndexCache[$cacheName] = 0;
+				} else {
+					return self::$arrayIndexCache[$cacheName];
+				}
+			}
+
+			$counter = 0;
+			$index = -1;
+			$tmp = array();
+
+			$arrayKey = NULL;
+			$arrayValue = NULL;
+
+			foreach ($array as $arrayKey => $arrayValue) {
+				if (($arrayValue == $value && $strictType == FALSE) || ($arrayValue === $value && $strictType == TRUE)) {
+					array_push($tmp, $arrayKey);
+
+					if ($collectIndexes == FALSE) break;
+				}
+			}
+
+			$index = (sizeof($tmp) == 1 ? $tmp[0] : ($collectIndexes ? $tmp : -1));
+
+			if (!empty($cacheName)) {
+				self::$arrayIndexCache[$cacheName] = $index;
+			}
+
+			return $index;
 		}
 
 		/*
@@ -73,6 +150,5 @@
 			return (STRIP) ? stripslashes($var) : $var;
 		}
 	}
-
 
 ?>
