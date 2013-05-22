eb8a568b48f53eed78dd4b02a0386b54cf6182f9
PatrickWe <weber.patrick91@gmail.com>
2013-05-22 23:02:55 +0300

    Update (C) IT-Talent.de

--- styles/standard/footer.php
+++ styles/standard/footer.php
@@ -5,7 +5,7 @@
 		</div>
 		
 		<footer>
-			<small>Forensoftware &copy; by <a target="_blank" href="http://www.itschi.net/">Itschi.Net</a></small>
+			<small>Forensoftware &copy; by <a target="_blank" href="http://www.it-talent.de/">IT-Talent.de</a></small>
 
 			<?php if ($user->row['user_level'] == ADMIN): ?>
 				<br /><small><b><a href="admin/index.php">Adminpanel</a></b></small>
