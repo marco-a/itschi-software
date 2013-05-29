b8486d7d5e527b0398233bb24aad0f8f653d3848
bluefirex <mail@gidix.de>
2013-05-29 21:54:44 +0200

    another redelcare fix

--- lib/plugins/HTTP/HTTPResponse.class.php
+++ lib/plugins/HTTP/HTTPResponse.class.php
@@ -6,58 +6,59 @@
 		< HTTPResponse Class >
 	**/
 
-	final class HTTPResponse implements HTTPResponseInterface {
+	if (!class_exists('HTTPResponse')) {
+		final class HTTPResponse implements HTTPResponseInterface {
 
-		/*
-			@name	locked
-		*/
-		private $locked = NULL;
+			/*
+				@name	locked
+			*/
+			private $locked = NULL;
 
-		/*
-			@name	items
-		*/
-		private $items = NULL;
+			/*
+				@name	items
+			*/
+			private $items = NULL;
 
-		/*
-			@name	__construct
-		*/
-		public function __construct() {
-			$this->locked = false;
-			$this->items = array();
+			/*
+				@name	__construct
+			*/
+			public function __construct() {
+				$this->locked = false;
+				$this->items = array();
+			}
+
+			/*
+				@name	setItem
+				sets an item
+			*/
+			public function setItem($name, $value) {
+				if ($this->locked == true || isset($this->items[$name])) return false;
+
+				$this->items[$name] = $value;
+
+				return true;
+			}
+
+			/*
+				@name	lock
+				locks instance
+			*/
+			public function lock() {
+				$this->locked = true;
+			}
+
+			/*
+				@name	__call
+			*/
+			public function __call($method, $args) {
+				$item = str_replace('get', '', $method);
+				$item = lcfirst($item);
+
+				if (!isset($this->items[$item])) return NULL;
+
+				return $this->items[$item];
+			}
+
 		}
-
-		/*
-			@name	setItem
-			sets an item
-		*/
-		public function setItem($name, $value) {
-			if ($this->locked == true || isset($this->items[$name])) return false;
-
-			$this->items[$name] = $value;
-
-			return true;
-		}
-
-		/*
-			@name	lock
-			locks instance
-		*/
-		public function lock() {
-			$this->locked = true;
-		}
-
-		/*
-			@name	__call
-		*/
-		public function __call($method, $args) {
-			$item = str_replace('get', '', $method);
-			$item = lcfirst($item);
-
-			if (!isset($this->items[$item])) return NULL;
-
-			return $this->items[$item];
-		}
-
 	}
-
 ?>
