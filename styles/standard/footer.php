032f9663aee03a7bcf539642c07237e51854dd23
bluefirex <mail@gidix.de>
2013-07-10 19:27:43 +0200

    added version number to footer

--- styles/standard/footer.php
+++ styles/standard/footer.php
@@ -5,7 +5,7 @@
 		</div>
 		
 		<footer>
-			<small>Forensoftware &copy; by <a target="_blank" href="http://www.it-talent.de/">IT-Talent.de</a></small>
+			<small>Forensoftware &copy; by <a target="_blank" href="http://www.it-talent.de/">IT-Talent.de</a> &minus; 3.0-pre1307</small>
 
 			<?php if ($user->row['user_level'] == ADMIN): ?>
 				<br /><small><b><a href="admin/index.php">Adminpanel</a></b></small>
