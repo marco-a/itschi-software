2d32791becd8eb38184e22d0d080e0a0200990fd
bluefirex <mail@gidix.de>
2013-08-02 12:12:43 +0200

    example on how to use data functions

--- plugins/com.gidix.examplePlugin/files/main.php
+++ plugins/com.gidix.examplePlugin/files/main.php
@@ -1,5 +1,27 @@
 <?php
 	/** main file for plugins **/
+	// This is only an example.
 
-	
+	/* $userData = plugin::data()->getUserByName('putSomeNameHere');
+
+	if ($userData instanceof UserData) {
+		$content = $userData->getUsername() . ' is ' . ($userData->isAdmin() ? 'admin' : 'something else') . '.';
+	} else {
+		$content = 'not found';
+	}
+
+	plugin::TPL()->addToArea('aboveContent', $content);
+
+	plugin::dataWritable()->updateUserByID($userData->getUserID(), new UserDataWritable(
+		$userData->getUsername(),
+		$userData->getAvatar(),
+		$userData->getEmailAdress(),
+		$userData->getLevel(),
+		$userData->getSignature(),
+		$userData->getStatus(),
+		$userData->getPoints(),
+		$userData->isBanned(),
+		$userData->getWebsite(),
+		$userData->getSkype()
+	)); */
 ?>
